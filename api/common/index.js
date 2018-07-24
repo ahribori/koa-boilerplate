5c2cc31e02e331aec154723ea5785b52ee36b76f
ahribori <ahribori@gmail.com>
2018-07-25 00:11:52 +0900

    비동기 작업과 조건에 따른 응답 테스트

--- api/common/index.js
+++ api/common/index.js
@@ -3,7 +3,23 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.get('/', async (ctx, next) => {
-    ctx.body = 'ok';
+    const { request: req } = ctx;
+
+    if (req.header.authorization !== 'token') {
+        ctx.status = 403;
+        ctx.body = 'forbidden!';
+        return;
+    }
+
+    const result = await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('ok!');
+        }, 1000);
+    });
+
+    ctx.body = {
+        result,
+    };
 });
 
 router.get('/user/:id', async (ctx, next) => {
